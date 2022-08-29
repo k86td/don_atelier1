1c119e011d86d965f4dbfdcca80c017b7dbc9fc8
Tristan Lepine <tristanlepine14@gmail.com>
2022-08-28 23:55:48 -0400

    commiting base files

---
+++ server.js
@@ -0,0 +1,40 @@
+
+const http = require('http');
+const queryString = require('query-string');
+
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+	let reqInfo = { url: req.url, method: req.method, contentType: req.headers['content-type'] };
+
+	res.writeHead(200, { "Content-Type": "application/json" });
+	if (req.method == 'GET') {
+		res.end(JSON.stringify(reqInfo));
+	} else {
+		if (req.method == 'POST') {
+			let body = [];
+			req.on('data', chunk => {
+				body.push(chunk);
+			}).on('end', () => {
+				try {
+					if (req.headers['content-type'] === "application/json")
+						reqInfo.body = JSON.parse(body);
+					else
+					if (req.headers['content-type'] === "application/x-www-form-urlencoded")
+						reqInfo.body = queryString.parse(body.toString());
+					else
+						reqInfo.body = body.toString();
+					res.end(JSON.stringify(reqInfo));
+				} catch (error) {
+					console.log(error);
+				}
+			});
+		}
+	}
+});
+
+// listen to PORT
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
+
